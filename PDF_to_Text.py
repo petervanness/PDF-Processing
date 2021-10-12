7b9c474b6045d47381ae25b506d28c4f78b14b2d
petervanness <82836675+petervanness@users.noreply.github.com>
2021-10-11 20:27:22 -0400

    Update PDF_to_Text.py

--- PDF_to_Text.py
+++ PDF_to_Text.py
@@ -1,12 +1,11 @@
-def pull_keywords(keywords, text, filename):
-    out_df = pd.DataFrame(columns=['Keyword','Reference','File'])
-    for keyword in keywords:
-        k = keyword.lower()
-        t = text.lower()
-        res = [i.start() for i in re.finditer(k, t)]
-        if len(res)>0:
-            print(keyword +' found '+str(len(res))+' time(s) in '+str(filename))
-        for i in res:
-            tmp = pd.DataFrame({'Keyword':[keyword],'Reference':[text[i-100:i+100].replace("\n"," ")],'File':[filename]})
-            out_df = out_df.append(tmp)
-    return out_df
+def pdf_to_text(in_file):
+    output_string = io.StringIO()
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+    text = output_string.getvalue()
+    return text

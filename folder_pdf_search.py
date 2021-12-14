258881de26e33107fd40763f5b6fa7d1bd964073
petervanness <82836675+petervanness@users.noreply.github.com>
2021-12-14 10:11:17 -0500

    eliminate duplicated function

--- folder_pdf_search.py
+++ folder_pdf_search.py
@@ -20,22 +20,6 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
 
-def pull_keywords(keywords, text, filename):
-    out_df = pd.DataFrame(columns=['Keyword','Reference','File','Full Path'])
-    for keyword in keywords:
-        k = keyword.lower()
-        t = text.lower()
-        res = [i.start() for i in re.finditer(k, t)]
-        if len(res)==1:
-            print('--'+keyword +' found '+str(len(res))+' time in '+str(filename.split('/')[-1]))
-        elif len(res)>1:
-            print('--'+keyword +' found '+str(len(res))+' times in '+str(filename.split('/')[-1]))
-        for i in res:
-            tmp = pd.DataFrame({'Keyword':[keyword],'Reference':[text[i-50:i+50].replace("\n"," ")],'File':[str(filename.split('/')[-1])],'Full Path':[filename]})
-            out_df = out_df.append(tmp)
-    return out_df
-
-
 def pull_pdfs_folder(in_folder, keywords, out_path):
     out_df = pd.DataFrame(columns=['Keyword','Reference','File','Full Path'])
     not_read = []

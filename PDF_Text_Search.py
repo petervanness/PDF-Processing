eb4b1220a82c2dfcaa66529a79bc19ac22f08528
petervanness <82836675+petervanness@users.noreply.github.com>
2021-12-14 10:06:30 -0500

    More precise printing and full path for references
    Add if statement for just one instance of keyword so that it prints "found 1 time" rather than "found 1 time(s)." Unnecessary but why not. Also added full file path of PDFs with references to output for easier navigation when reviewing results.

--- PDF_Text_Search.py
+++ PDF_Text_Search.py
@@ -4,9 +4,11 @@ def pull_keywords(keywords, text, filename):
         k = keyword.lower()
         t = text.lower()
         res = [i.start() for i in re.finditer(k, t)]
-        if len(res)>0:
-            print(keyword +' found '+str(len(res))+' time(s) in '+str(filename))
+        if len(res)==1:
+            print('--'+keyword +' found '+str(len(res))+' time in '+str(filename.split('/')[-1]))
+        elif len(res)>1:
+            print('--'+keyword +' found '+str(len(res))+' times in '+str(filename.split('/')[-1]))
         for i in res:
-            tmp = pd.DataFrame({'Keyword':[keyword],'Reference':[text[i-100:i+100].replace("\n"," ")],'File':[filename]})
+            tmp = pd.DataFrame({'Keyword':[keyword],'Reference':[text[i-50:i+50].replace("\n"," ")],'File':[str(filename.split('/')[-1])],'Full Path':[filename]})
             out_df = out_df.append(tmp)
     return out_df
